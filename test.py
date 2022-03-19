024de00d7df0adfb074a803de0d5332e47f8b75e
Elias Ojeda Medina <eliasojedamedina@gmail.com>
2022-03-18 22:26:44 -0600

    fix: delete test file

--- test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import datetime
-
-df = pd.read_csv('backtesting/ETHUSDT-1m-result.csv')
-current_minute = datetime.datetime.now().minute
-df_minute = pd.to_datetime(df.tail(1)['timestamp'])
-
-print(current_minute, df_minute, current_minute)
