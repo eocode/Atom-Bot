c7d423324a1eee639ff5e491539dd75291a6c1f5
Elias Ojeda Medina <eliasojedamedina@gmail.com>
2022-03-20 13:08:07 -0600

    fix: update message logging

--- modules/financing/crypto/algorithms/trader.py
+++ modules/financing/crypto/algorithms/trader.py
@@ -124,18 +124,18 @@ class CryptoBot:
 
                             last_row = df.iloc[-1, :]
                             self.save_trade(last_row=last_row, size=size)
-                            message = "%s %s" % (
-                                self.symbol, convert_utc_to_local(str(self.trades['micro']['1m']['fingerprint'])))
-                            message += " 1m   - RSI %s | " % self.trades['micro']['1m']['trade']['RSI']
+                            message = " 1m   - RSI %s | " % self.trades['micro']['1m']['trade']['RSI']
                             message += " 5m  - RSI %s | " % self.trades['micro']['5m']['trade']['RSI']
                             message += " 15m - RSI %s | " % self.trades['short']['15m']['trade']['RSI']
-                            message += " 30m - RSI %s Momentum %s | " % (
+                            message += " 30m - RSI %s Momentum %s \n" % (
                                 self.trades['short']['30m']['trade']['RSI'],
                                 self.trades['short']['30m']['trade']['Momentum'])
                             message += " 1h  - RSI %s | " % self.trades['medium']['1h']['trade']['RSI']
                             message += " 4h  - RSI %s" % self.trades['medium']['4h']['trade']['RSI']
                             logging_message(message)
                             self.take_decision(testing=False)
+                    message = "Actualizado a: %s %s" % (self.symbol, convert_utc_to_local(str(self.trades['micro']['1m']['fingerprint'])))
+                    logging_message(message)
                     self.first_iteration = True
                 except Exception as e:
                     print('Error: ', e)
